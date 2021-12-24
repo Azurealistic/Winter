ed8fd89ebbd1e68310cc78b774deac55bcfb5618
Muhammmad Asavir <asavirm@gmail.com>
2021-12-24 13:23:59 -0500

    Day 23, 2021.

---
+++ Solutions/2021/23.py
@@ -0,0 +1,18 @@
+# Advent of Code - Day:
+# Imports (Always imports data based on the folder and file name)
+from aocd import data, submit
+
+def solve():
+	# Did this completely by hand.
+	print("Star 1:", 14346)
+	print("Star 2:", 48984)
+	submit(14346, part="a", day=23, year=2021)
+	submit(48984, part="b", day=23, year=2021)
+
+# Solution
+def main():
+	solve()
+
+# Call the main function.
+if __name__ == '__main__':
+	main()
